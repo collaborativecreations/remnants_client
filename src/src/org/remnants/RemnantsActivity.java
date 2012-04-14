fd9b1c349ce5214596b98ca334fd9d7b3f2c41b7
Brendan Powers <brendan@resara.com>
2012-04-14 13:44:35 -0400

    Removed the bin/ directory Added a service

--- src/src/org/remnants/RemnantsActivity.java
+++ src/src/org/remnants/RemnantsActivity.java
@@ -1,6 +1,7 @@
 package org.remnants;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class RemnantsActivity extends Activity {
@@ -9,5 +10,8 @@ public class RemnantsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Intent intent = new Intent(this, RemnantsService.class);
+        startService(intent);
     }
 }
