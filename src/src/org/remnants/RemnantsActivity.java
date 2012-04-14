5b47fd0d569404539a2de3b713ef89b3313e0abc
Brendan Powers <brendan@resara.com>
2012-04-14 13:20:36 -0400

    Added example android project

---
+++ src/src/org/remnants/RemnantsActivity.java
@@ -0,0 +1,13 @@
+package org.remnants;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RemnantsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
